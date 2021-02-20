f55c1256239608d04cb4bfe96f7b3b3de059c32c
mankokolya <mankokolya@gmail.com>
2021-02-20 16:24:38 +0200

    added difficulty enum to recipe pojo

--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -16,8 +16,11 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+
     @Lob
     private Byte[] image;
 
@@ -27,6 +30,13 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
