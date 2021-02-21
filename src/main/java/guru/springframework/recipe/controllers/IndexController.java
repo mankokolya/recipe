282ab0ad1528d203c61b183000c38b63dd5be33c
mankokolya <mankokolya@gmail.com>
2021-02-21 15:56:56 +0200

    using setters for jpa Bidirectional relationships

--- src/main/java/guru/springframework/recipe/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipe/controllers/IndexController.java
@@ -1,15 +1,9 @@
 package guru.springframework.recipe.controllers;
-
-import guru.springframework.recipe.domain.Category;
-import guru.springframework.recipe.domain.UnitOfMeasure;
-import guru.springframework.recipe.repositories.CategoryRepository;
-import guru.springframework.recipe.repositories.UnitOfMeasureRepository;
 import guru.springframework.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
 @Controller
 public class IndexController {
