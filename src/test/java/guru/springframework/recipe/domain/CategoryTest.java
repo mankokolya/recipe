cc1946d853f53aefd2bf0f1f973b7f40c3332760
mankokolya <mankokolya@gmail.com>
2021-02-23 15:21:05 +0200

    tests for recipeServiceImpl

---
+++ src/test/java/guru/springframework/recipe/domain/CategoryTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.recipe.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+    Category category;
+
+    @Before
+    public void setUp() {
+      category = new Category();
+    }
+    @Test
+    public void getId(){
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+}
